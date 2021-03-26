99a7d69c2906529755460b9a913b399daeb54c02
Eli Kaplan <elikaplan@me.com>
2021-03-26 01:58:52 +0000

    added some logic to prevent XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,12 +7,18 @@
 const createTweetElement = function(tweet){
   let date = new Date(tweet.created_at)
   
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+  
   const createdTweet = `<div class="box">
           <article class="tweet"> 
             <img class="profile-pic" src="${tweet.user.avatars}"> ${tweet.user.name} 
             <a class="handle">${tweet.user.handle}</a> 
            </article>
-            <a class="content">${tweet.content.text}</a>
+            <a class="content">${escape(tweet.content.text)}</a>
           </div>
           <div class="footer">
             <footer>Tweeted on ${date.toDateString()} 
@@ -27,9 +33,10 @@ const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
+  $('#tweets-container').html("<container></container>")
   for(const tweet of tweets){
     const newTweet = createTweetElement(tweet)
-    $('#tweets-container').append(newTweet)
+    $('#tweets-container').prepend(newTweet)
   }
 
 }
@@ -40,7 +47,7 @@ const renderTweets = function(tweets) {
 $(document).ready(function(){
   const $button = $('form');
   const loadTweets = function(){
-    $('#tweets-container').html("<container></container>") 
+    //$('#tweets-container').html("<container></container>") 
     $.ajax('/tweets', { method: 'GET' })
   .then(function (data) {
     renderTweets(data);
