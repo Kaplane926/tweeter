1ca8e19cfa45aba1b2d24a90c18a4253f5e04b9e
Eli Kaplan <elikaplan@me.com>
2021-03-26 01:10:17 +0000

    added tweet fetching and rendering functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,36 +35,45 @@ const renderTweets = function(tweets) {
 }
 
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
 
 
 $(document).ready(function(){
-  renderTweets(data)
+  const $button = $('form');
+  const loadTweets = function(){
+    $('#tweets-container').html("<container></container>") 
+    $.ajax('/tweets', { method: 'GET' })
+  .then(function (data) {
+    renderTweets(data);
+  });
+}
+  $button.submit(function (event) {
+    event.preventDefault();
+    console.log('Button clicked, performing ajax call...');
+    //console.log($(this).serialize())
+    if($(this).serialize().length >= 140){
+      alert("You've exceeded the character limit!")
+    } else if ($(this).serialize().length === null){
+      alert("Man type something")
+    } else {
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $(this).serialize(),
+      /*success: function(resultData){
+          console.log("Its the data!",resultData)
+      }*/
+    }).then(()=>{
+      loadTweets();
 
-})
+    }).catch(()=>{
+      
+        alert("You're tweet must contain content.")
+      
+      console.log("error")
+    })
+  }
+    
+    })
+
+});
+
--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
   $(function(){
     $('#tweet-text').on('input', function(){ 
-      console.log($('#tweet-text').val().length)
       $('.counter').html(140 - $('#tweet-text').val().length)
       if($('.counter').val() < 0){
         $('.counter').css({
